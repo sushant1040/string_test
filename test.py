f09c891d9011a188b81c4bcb0209add6ce841abe
sushant1040 <use.sushantkumar@gmail.com>
2026-03-09 15:03:29 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,48 @@
+# 4
+string=  "sushant"
+string1 = string.__len__()
+print(string1)
+# 5
+string=  "sushant"
+string1 = string.upper()
+print(string1)
+# 6
+string=  "SUSHANT"
+string1 = string.lower()
+print(string1)
+# 13
+string=  "sushant and sushant's friend"
+string1 = string.count("sushant")
+print(string1)
+# 14
+string=  "sushant"
+string1 = string.isalpha()
+print(string1)
+
+# 15
+string=  "sushant"
+string1 = string.isnumeric()
+print(string1)
+
+
+# 7 
+
+string=  "sushant"
+string1 = string.replace("sushant","rohit")
+print(string1) 
+# 8
+
+string=  "i am having fun"
+string1 = string.split()
+print(string1)
+
+# 1
+string=  "sushant"
+string1 = string.__add__("nitish")
+print(string1)
+
+string=  "  sushant  "
+string1 = string.removeprefix("")
+print(string1)
+
+
